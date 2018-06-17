a90b28cf0afd4e98f8aee549e953505434999b41
DarkEye <lesko.matej.pu@gmail.com>
2018-06-17 17:20:47 +0200

    example of communication using events

---
+++ components/simple-component-parent-child-communication/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 mx-auto">
+        <user></user>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import User from './components/User.vue';
+  export default {
+    components:{
+      User
+    }
+  }
+</script>
+
+<style scoped>
+  div.component {
+    border: 2px solid black;
+    padding: 30px;
+  }
+</style>
---
+++ components/simple-component-parent-child-communication/src/components/UserDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="component">
+    <h5>You may view your User Details here</h5>
+    <p>Many Details</p>
+    <p>Current username: {{ sharedName }}</p>
+    <hr>
+    <button @click="fireEvent()">Change the name</button>
+   </div>
+</template>
+
+<script>
+  export default {
+    // props: ['sharedName']
+    props: {
+      sharedName: String,
+    },
+    methods: {
+      fireEvent() {
+        this.sharedName = "changedFromChild"
+        this.$emit("nameChange", this.sharedName)
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>
