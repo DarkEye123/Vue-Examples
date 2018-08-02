48b1f9499a407f1fa583f1881e9e4c84693daaed
DarkEye <lesko.matej.pu@gmail.com>
2018-08-02 22:20:32 +0200

    Quote component introduced with sloths

--- components/advanced/advanced-component/src/App.vue
+++ components/advanced/advanced-component/src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-xs-12">
-
+      <div class="col-xs-12 col-sm-6 mx-auto">
+        <app-quote>
+          <h1>A wonderful quote!</h1>
+          <p>A wonderful body!</p>
+        </app-quote>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import Quote from './components/Quote';
+  export default {
+    components: {
+      appQuote: Quote
+    }
+  };
 </script>
 
 <style scoped>
--- components/advanced/advanced-component/src/components/Quote.vue
+++ components/advanced/advanced-component/src/components/Quote.vue
@@ -1,5 +1,8 @@
 <template>
-
+    <div>
+        <slot></slot>
+    </div>
+    
 </template>
 
 <script>
@@ -9,6 +12,12 @@ export default {
 </script>
 
 <style scoped>
-
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px 10em auto;
+        text-align: center;
+    }
 </style>
 
