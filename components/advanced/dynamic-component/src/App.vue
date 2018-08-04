d61ab6ab7e957989e817cfb250e16c83fedd79ca
DarkEye <lesko.matej.pu@gmail.com>
2018-08-04 21:31:47 +0200

    Example of indestructible components

--- components/advanced/dynamic-component/src/App.vue
+++ components/advanced/dynamic-component/src/App.vue
@@ -5,10 +5,19 @@
         <button @click="selectedComponent = 'appQuote'">Quote</button>
         <button @click="selectedComponent = 'appAuthor'">Author</button>
         <button @click="selectedComponent = 'appNew'">New</button>
+        <h2>Destroyable components</h2>
         <p>{{ selectedComponent }}</p>
         <component :is="selectedComponent" v-bind:title="title">
           <p>Default Body</p>
         </component>
+        <hr>
+        <!-- Note that all is the same but components won't write to the log that they are destroyed -->
+        <h2>Persistent components</h2>
+        <keep-alive>
+          <component :is="selectedComponent" v-bind:title="title">
+            <p>Default Body of persistent component</p>
+          </component>
+        </keep-alive>
       </div>
     </div>
   </div>
