91fd829037f217b0062e759c277d121e2647119e
DarkEye <lesko.matej.pu@gmail.com>
2018-08-04 20:59:18 +0200

    Example of dynamic components + directory renaming

---
+++ components/advanced/dynamic-component/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 mx-auto">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <p>{{ selectedComponent }}</p>
+        <component :is="selectedComponent" v-bind:title="title">
+          <p>Default Body</p>
+        </component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote";
+import New from "./components/New";
+import Author from "./components/Author";
+export default {
+  data() {
+    return {
+      title: "A wonderful quote!",
+      selectedComponent: 'appQuote'
+    };
+  },
+  components: {
+    appQuote: Quote,
+    appNew: New,
+    appAuthor: Author
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ components/advanced/dynamic-component/src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['title']
+  }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px 10em auto;
+        text-align: center;
+    }
+    /* h1 is given in the parent */
+    h1 {
+        color: red
+    }
+</style>
+
