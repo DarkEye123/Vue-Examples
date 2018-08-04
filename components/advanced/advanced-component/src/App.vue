e710931175f1289829bc6fdc5f4723e47c0ca6c5
DarkEye <lesko.matej.pu@gmail.com>
2018-08-04 19:31:13 +0200

    Example of default slot with named slot and value assignment

--- components/advanced/advanced-component/src/App.vue
+++ components/advanced/advanced-component/src/App.vue
@@ -10,6 +10,15 @@
           <h1 slot="titledSlot">{{ title }}</h1>
           <p slot="content">A wonderful body of titled slot!</p>
         </app-quote-named>
+        <app-quote-named-default>
+          <h1 slot="titledSlot">{{ title }}</h1>
+          <!-- Here the span element will be filled with the rest of the elements (in this case <p></p>) -->
+          <p>A wonderful body of titled slot!</p>
+        </app-quote-named-default>
+        <app-quote-named-default>
+          <h1 slot="titledSlot">{{ title }}</h1>
+          <!-- Here is used defautl value of slot of the QuoteNamedSlotDefault -->
+        </app-quote-named-default>
       </div>
     </div>
   </div>
@@ -18,6 +27,7 @@
 <script>
   import Quote from './components/Quote-slot';
   import QuoteNamedSlot from './components/Quote-slot-named';
+  import QuoteNamedSlotDefault from './components/Quote-slot-named-with-default';
   export default {
     data() {
       return {
@@ -26,7 +36,8 @@
     },
     components: {
       appQuote: Quote,
-      appQuoteNamed: QuoteNamedSlot
+      appQuoteNamed: QuoteNamedSlot,
+      appQuoteNamedDefault: QuoteNamedSlotDefault
     }
   };
 </script>
---
+++ components/advanced/advanced-component/src/components/Quote-slot-named-with-default.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class=title>
+            <slot name="titledSlot"></slot>
+        </div>
+        <hr>
+        <div>
+            <span style="color: #ccc">
+                <slot>Showing default value without content from parent</slot>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px 10em auto;
+        text-align: center;
+    }
+    /* h1 is given in the parent */
+    h1 {
+        color: red
+    }
+    .title {
+        font-style: italic
+    }
+</style>
+
