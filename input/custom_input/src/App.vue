c9e4c4f3c0c3343efa0f774d24ad6f766b9183ae
matej lesko <lesko.matej.pu@gmail.com>
2018-08-16 20:57:21 +0200

    Example of custom input

---
+++ input/custom_input/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <app-switcher v-model="switcherValue"></app-switcher>
+    <hr>
+    <p>{{switcherValue}}</p>
+  </div>
+</template>
+
+<script>
+  import Switcher from './components/Switcher';
+  export default {
+    data() {
+      return {
+        switcherValue: 'off',
+      }
+    },
+    components: {
+      appSwitcher: Switcher,
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ input/custom_input/src/components/Switcher.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div id="on" :class="{active: value == 'on'}" @click="switchValue('on')">
+      on
+    </div>
+
+    <div id="off" :class="{active: value != 'on'}" @click="switchValue('off')">
+      off
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    methods: {
+      switchValue(val) {
+        this.$emit("input", val);
+        console.log(this.value, val);
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  #on, #off {
+    width: 40px;
+    height: 20px;
+    display: inline-block;
+    margin: 10px -1px;
+    background-color: whitesmoke;
+    text-align: center;
+    border: 1px solid black;
+    cursor: pointer;
+    padding: 2px;
+  }
+  #on:hover, #on.active {
+    background-color: lightgreen;
+  }
+
+  #off:hover, #off.active {
+    background-color: lightcoral;
+  }
+</style>
