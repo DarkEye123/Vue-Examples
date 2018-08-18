08d20f1ab9ab8c90e9ece7cf644c23dc00733b49
matej lesko <lesko.matej.pu@gmail.com>
2018-08-18 14:36:55 +0200

    Directive parametrized

--- directives/simple_directive/src/App.vue
+++ directives/simple_directive/src/App.vue
@@ -4,7 +4,8 @@
     <p v-highlight="'red'">my own directive with value</p>
     <p v-highlight:textColor="'red'">my own directive with argument</p>
     <p v-highlight:textColor.delayed="'lightblue'">my own directive with argument and modifier</p>
-    <p v-local-highlight:textColor.delayed.blink="'orange'">my own LOCAL directive with argument and modifiers</p>
+    <p v-local-highlight:textColor.blink="{color:'blue', delayed:1000}">my own LOCAL directive with argument and modifiers</p>
+    <p v-local-highlight:textColor.blink="{tcolor:'lightcoral', delayed:500}">my own LOCAL directive with argument and modifiers</p>
   </div>
 </template>
 
@@ -13,29 +14,23 @@ export default {
   directives: {
     "local-highlight": {
       bind(el, binding, vnode) {
-        let delay = 0;
-        if (binding.modifiers["delayed"]) {
-          delay = 1000;
-        }
+        let delay = binding.value["delayed"] || 0;
+        let currentColor = binding.value["color"] || "lightblue";
+        let nextColor = binding.value["tcolor"] || 'red';
+        el.style.color = currentColor;
         if (binding.modifiers["blink"]) {
-          let currentColor = binding.value;
-          let nextColor = 'red';
           let previousColor = currentColor;
-          el.style.color = currentColor;
           setInterval(() => {
-            currentColor = currentColor == nextColor ? previousColor : nextColor;
+            currentColor =
+              currentColor == nextColor ? previousColor : nextColor;
             el.style.color = currentColor;
           }, delay);
         } else {
           setTimeout(() => {
             if (binding.arg == "textColor") {
-              el.style.color = binding.value;
+              el.style.color = currentColor;
             } else {
-              if (!binding.value) {
-                el.style.backgroundColor = "lightgreen";
-              } else {
-                el.style.backgroundColor = binding.value;
-              }
+              el.style.backgroundColor = binding.value;
             }
           }, delay);
         }
