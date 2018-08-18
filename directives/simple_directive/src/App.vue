590347522eaa7f80530fb20fcb182cabd799e109
matej lesko <lesko.matej.pu@gmail.com>
2018-08-18 12:35:23 +0200

    modifier introduction

--- directives/simple_directive/src/App.vue
+++ directives/simple_directive/src/App.vue
@@ -3,6 +3,7 @@
     <p v-highlight>my own directive</p>
     <p v-highlight="'red'">my own directive with value</p>
     <p v-highlight:textColor="'red'">my own directive with argument</p>
+    <p v-highlight:textColor.delayed="'lightblue'">my own directive with argument and modifier</p>
   </div>
 </template>
 
--- directives/simple_directive/src/main.js
+++ directives/simple_directive/src/main.js
@@ -1,17 +1,25 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+// Note that setTimeout delays styling even if there is delay = 0. It is not the best performance solution. Only example
 Vue.directive('highlight', {
     bind(el, binding, vnode) {
-        if (binding.arg == 'textColor') {
-            el.style.color = binding.value;
-        } else {
-            if (!binding.value) {
-                el.style.backgroundColor = 'lightgreen';
-            } else {
-                el.style.backgroundColor = binding.value;
-            }
+        let delay = 0;
+        if (binding.modifiers['delayed']) {
+            delay = 3000;
         }
+        setTimeout(() => {
+            if (binding.arg == 'textColor') {
+                el.style.color = binding.value;
+            } else {
+                if (!binding.value) {
+                    el.style.backgroundColor = 'lightgreen';
+                } else {
+                    el.style.backgroundColor = binding.value;
+                }
+            }
+
+        }, delay);
     }
 });
 
