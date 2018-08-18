01fa6dafe7f113b20b11efe9f22b3e7b8f8def60
matej lesko <lesko.matej.pu@gmail.com>
2018-08-18 11:23:45 +0200

    eslintrc + directive accepts value

--- directives/simple_directive/src/App.vue
+++ directives/simple_directive/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <p v-highlight>my own directive</p>
+    <p v-highlight="'red'">my own directive with value</p>
   </div>
 </template>
 
--- directives/simple_directive/src/main.js
+++ directives/simple_directive/src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-Vue.directive("highlight", {
+Vue.directive('highlight', {
     bind(el, binding, vnode) {
-        el.style.backgroundColor = "lightgreen";
+        if (!binding.value) {
+            el.style.backgroundColor = 'lightgreen';
+        } else {
+            el.style.backgroundColor = binding.value;
+        }
     }
-})
+});
 
 new Vue({
     el: '#app',
     render: h => h(App)
-})
+});
