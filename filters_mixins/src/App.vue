9e177597c032481dff4ab884d62f9418ae8aea61
DarkEye <lesko.matej.pu@gmail.com>
2018-08-23 20:48:34 +0200

    Example of filters(local, global) + usage case where computed is better

---
+++ filters_mixins/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p>{{text | toUpper | filter-amp}}</p>
+    <hr>
+    <input v-model="iText" placeholder="type for filtering">
+    <ul>
+      <li v-for="(element, index) in filterElements" :key=index>{{element}}</li> 
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "app",
+  data() {
+    return {
+      text: "example text",
+      iText: "",
+      betterUseComputed: ["dore", "remi", "mifa", "faso", "sola"]
+    };
+  },
+  filters: {
+    toUpper(data) {
+      return data.toUpperCase();
+    }
+  },
+  computed: {
+    filterElements() {
+      return this.betterUseComputed.filter(element =>
+        element.match(this.iText)
+      );
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ filters_mixins/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Filter that is globally registered
+Vue.filter("filter-amp", (data) => {
+  return data.replace(/amp/gi, "");
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
