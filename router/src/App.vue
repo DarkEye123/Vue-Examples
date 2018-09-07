652b94f9a5a3535ce0b16d5eb983cdd124050e01
DarkEye <lesko.matej.pu@gmail.com>
2018-09-07 19:49:22 +0200

    router example with the navigation bar

---
+++ router/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 text-center">
+        <h1>Routing</h1>
+        <app-header></app-header>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    appHeader: Header
+  }
+};
+</script>
+
---
+++ router/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- note that previous usage with ul element and tag=li made active-class behavior very strange, therefore I used this approach -->
+    <nav class="nav nav-pills nav-fill">
+        <router-link class="nav-item nav-link" to="/" exact active-class="active">
+            Home
+        </router-link>
+        <router-link class="nav-item nav-link" to="/user" active-class="active">
+            User
+        </router-link>
+    </nav>
+</template>
