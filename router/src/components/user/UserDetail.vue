454e2286c96fed6f01b42ddb05d319914921d7f1
DarkEye <lesko.matej.pu@gmail.com>
2018-09-15 16:09:36 +0200

    query example

--- router/src/components/user/UserDetail.vue
+++ router/src/components/user/UserDetail.vue
@@ -3,7 +3,7 @@
     <h3>Some User Details</h3>
     <p class="mt-3 text-right">Id from the URL: {{id}}</p>
     <router-link class="btn btn-primary mr-3" :to="'/user/' + $route.params.id + '/edit'">edit using naive routing</router-link>
-    <router-link class="btn btn-primary" :to="{name: 'userEdit', params:{id: id}}">edit using named route</router-link>
+    <router-link class="btn btn-primary" :to="{name: 'userEdit', params:{id: id}, query: {locale: 'en', cnt: 100}}">edit using named route</router-link>
   </div>
 </template>
 
--- router/src/components/user/UserEdit.vue
+++ router/src/components/user/UserEdit.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Edit the User {{$route.params.id}}</h3>
+    <div>
+        <h3>Edit the User {{$route.params.id}}</h3>
+        <p>Locale: {{$route.query.locale}}</p>
+        <p>Count: {{$route.query.cnt}}</p>
+    </div>
 </template>
